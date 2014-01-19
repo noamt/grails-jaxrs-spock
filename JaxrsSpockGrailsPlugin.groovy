e3f95349a98a225962210c7889152c1915f602b1
noamt <noam@10ne.org>
2014-01-19 18:04:43 +0200

    Initial commit

---
+++ JaxrsSpockGrailsPlugin.groovy
@@ -0,0 +1,59 @@
+class JaxrsSpockGrailsPlugin {
+    def version = "0.0.1-grails2.0-SNAPSHOT"
+
+    def grailsVersion = "2.0 > *"
+
+    def dependsOn = [:]
+
+    def pluginExcludes = [
+            "grails-app/views/error.gsp"
+    ]
+
+    def title = "JAX-RS Spock Plugin"
+    def author = "Noam Y. Tenne"
+    def authorEmail = "noam@10ne.org"
+    def description = '''\
+Provides Spock specification support for the Grails JAX-RS plugin
+'''
+
+    def documentation = "http://grails.org/plugin/jaxrs-spock"
+
+    def license = "APACHE"
+
+    def organization = [name: '10ne.org', url: 'http://www.10ne.org/']
+
+    def issueManagement = [system: "github", url: "https://github.com/noamt/grails-jaxrs-spock/issues"]
+
+    def scm = [url: "https://github.com/noamt/grails-jaxrs-spock.git"]
+
+    def doWithWebDescriptor = { xml ->
+        // TODO Implement additions to web.xml (optional), this event occurs before
+    }
+
+    def doWithSpring = {
+        // TODO Implement runtime spring config (optional)
+    }
+
+    def doWithDynamicMethods = { ctx ->
+        // TODO Implement registering dynamic methods to classes (optional)
+    }
+
+    def doWithApplicationContext = { applicationContext ->
+        // TODO Implement post initialization spring config (optional)
+    }
+
+    def onChange = { event ->
+        // TODO Implement code that is executed when any artefact that this plugin is
+        // watching is modified and reloaded. The event contains: event.source,
+        // event.application, event.manager, event.ctx, and event.plugin.
+    }
+
+    def onConfigChange = { event ->
+        // TODO Implement code that is executed when the project configuration changes.
+        // The event is the same as for 'onChange'.
+    }
+
+    def onShutdown = { event ->
+        // TODO Implement code that is executed when the application shuts down (optional)
+    }
+}
---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,47 @@
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+
+grails.project.dependency.distribution = {
+    remoteRepository(id: 'snapshots-repo', url: 'http://noams.artifactoryonline.com/noams/grails-jaxrs-plugin-snapshots') {
+        authentication username: System.getProperty('DEPLOYER_USERNAME'), password: System.getProperty('DEPLOYER_PASSWORD')
+    }
+}
+
+grails.project.dependency.resolution = {
+    // inherit Grails' default dependencies
+    inherits("global") {
+        // uncomment to disable ehcache
+        excludes 'hibernate'
+    }
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    repositories {
+        grailsCentral()
+        // uncomment the below to enable remote dependency resolution
+        // from public Maven repositories
+        //mavenLocal()
+        //mavenCentral()
+        mavenRepo "http://noams.artifactoryonline.com/noams/grails-jaxrs-plugin-snapshots"
+        //mavenRepo "http://snapshots.repository.codehaus.org"
+        //mavenRepo "http://repository.codehaus.org"
+        //mavenRepo "http://download.java.net/maven/2/"
+        //mavenRepo "http://repository.jboss.com/maven2/"
+    }
+    dependencies {
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+    }
+
+    plugins {
+        build(":release:2.2.1",
+                ":rest-client-builder:1.0.3") {
+            export = false
+        }
+
+        compile(':jaxrs:0.9-SNAPSHOT') {
+            export = false
+        }
+        compile(':spock:0.7') {
+            export = false
+        }
+    }
+}
